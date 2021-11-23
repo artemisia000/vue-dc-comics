6cc1ce50cda17e38427c1bfab0169cc12e2820b3
artemisia000 <misiapierobon@gmail.com>
2021-11-23 16:26:33 +0100

    img

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+   <header>
+        <div>
+            <img src="" alt="">
+        </div>
+   </header>
+  
+</template>
+
+<script>
+export default {
+    name: 'header'
+
+}
+</script>
+
+<style>
+
+</style>
